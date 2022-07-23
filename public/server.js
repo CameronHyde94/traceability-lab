c2361236a2a856321f8f349bdb1241edb3852f69
Cameron Hyde <camhyde15@gmail.com>
2022-07-22 23:56:30 -0600

    added endpoints

--- public/server.js
+++ public/server.js
@@ -11,9 +11,41 @@ const characters = ['Mac', 'Dennis', 'Charlie']
 //Endpoints
 
 app.get('/', (req, res) => {
-    res.sendStatus(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.get('/api/characters', (req, res) => {
     res.status(200).send(characters)
 });
+
+app.post('/api/characters', (req, res) => {
+    let {name} = req.body
+
+    const index = characters.findIndex(character => {
+        return character === name
+    })
+
+    try {
+        if (index === -1 && name !== '') {
+            characters.push(name)
+            res.status(200).send(characters)
+        } else if (name === '') {
+            res.status(400).send('You must enter a name')
+        } else {
+            res.status(400).send('That character already exists')
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+app.delete('/api/characters/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    characters.splice(targetIndex, 1)
+    res.status(200).send(characters)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
